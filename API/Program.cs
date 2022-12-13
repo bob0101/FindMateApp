af6c69afa19376e417dd01bbb3d363cc234de46a
babti kane <babti.ak@gmail.com>
2022-12-12 20:21:08 -0500

    endsection2

---
+++ API/Program.cs
@@ -0,0 +1,33 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
+builder.Services.AddDbContext<DataContext>( opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+//Configure the HTTP request pipeline. pour le moment on a pas besoin de swagger midleware dans le pipeLine
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+/// pour le moment on a pas besoin de authentification midleware dans le pipeLine
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
